c102e07941fbe8bf8b5fd606f3cf41b94a71b171
YEJIN-LILY <gristar98@naver.com>
2021-11-24 08:56:37 +0900

    3일차: 엔티티 클래스 개발2

--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -1,5 +1,8 @@
 package jpabook.jpashop.domain.item;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.DiscriminatorColumn;
 import javax.persistence.Entity;
@@ -7,6 +10,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Inheritance;
 import javax.persistence.InheritanceType;
+import javax.persistence.ManyToMany;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -15,7 +19,7 @@ import lombok.Setter;
 @Getter @Setter
 @Inheritance(strategy=InheritanceType.SINGLE_TABLE)
 @DiscriminatorColumn(name="dtype")
-public abstract class Item {
+public abstract class Item{
 	@Id @GeneratedValue
 	@Column(name="item_id")
 	private Long id;
@@ -23,4 +27,7 @@ public abstract class Item {
 	private String name;
 	private int price;
 	private int stockQuantity;
+	
+	@ManyToMany(mappedBy="items")
+	private List<Category> categories=new ArrayList<>();
 }
