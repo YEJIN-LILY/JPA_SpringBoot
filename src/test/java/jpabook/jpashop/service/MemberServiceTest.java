e698f2d4e8f68cf388fa4a43f2030a5ef250c554
YEJIN-LILY <gristar98@naver.com>
2021-11-27 16:35:36 +0900

    3일차: 상품 도메인 개발

---
+++ src/test/java/jpabook/jpashop/service/MemberServiceTest.java
@@ -0,0 +1,55 @@
+package jpabook.jpashop.service;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import javax.persistence.EntityManager;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.domain.repository.MemberRepository;
+import jpabook.jpashop.domain.service.MemberService;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Transactional
+public class MemberServiceTest {
+
+	@Autowired MemberService memberService;
+	@Autowired MemberRepository memberRepository;
+	@Autowired EntityManager em;
+	
+	@Test
+	@Rollback(false)
+	public void 회원가입() throws Exception{
+		Member member=new Member();
+		member.setName("kim");
+		
+		Long saveId=memberService.join(member);
+		
+		em.flush();
+		assertEquals(member, memberRepository.findOne(saveId));
+	}
+	
+	@Test(expected=IllegalStateException.class)
+	public void 중복_회원_예외() throws Exception{
+		Member member1=new Member();
+		member1.setName("kim1");
+		
+		Member member2=new Member();
+		member1.setName("kim");
+		
+		memberService.join(member1);
+		memberService.join(member2); //예외가 발생해야 
+		
+		fail("예외가 발생해야 한다.");
+	}
+}
