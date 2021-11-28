8df970750b3e4727c58d7469d78e087d71a5cafc
YEJIN-LILY <gristar98@naver.com>
2021-11-28 18:44:19 +0900

    4일차: API 개발 고급-컬렉션 조회 최적화

--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -3,8 +3,11 @@ package jpabook.jpashop.repository;
 import java.util.List;
 
 import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
 
 import org.springframework.stereotype.Repository;
+import org.springframework.util.StringUtils;
+
 
 import jpabook.jpashop.domain.Order;
 import lombok.RequiredArgsConstructor;
@@ -24,13 +27,67 @@ public class OrderRepository {
 	}
 	
 	public List<Order> findAll(OrderSearch orderSearch){
-		return em.createQuery("select o from Order o join o.member m"+
-				" where o.status=:status"+
-				" and m.name like :name"
-				,Order.class)
-				.setParameter("status",orderSearch.getOrderStatus())
-				.setParameter("name",orderSearch.getOrderStatus())
-				.setMaxResults(1000)
-				.getResultList();
+		//language=JPAQL
+	      String jpql = "select o From Order o join o.member m";
+	      boolean isFirstCondition = true;
+	//주문 상태 검색
+	if (orderSearch.getOrderStatus() != null) {
+	          if (isFirstCondition) {
+	              jpql += " where";
+	              isFirstCondition = false;
+	          } else {
+	              jpql += " and";
 	}
+	          jpql += " o.status = :status";
+	      }
+	//회원 이름 검색
+	if (StringUtils.hasText(orderSearch.getMemberName())) {
+	          if (isFirstCondition) {
+	              jpql += " where";
+	              isFirstCondition = false;
+	          } else {
+	              jpql += " and";
+	}
+	          jpql += " m.name like :name";
+	      }
+	  
+	 TypedQuery<Order> query = em.createQuery(jpql, Order.class) .setMaxResults(1000); //최대 1000건
+	      if (orderSearch.getOrderStatus() != null) {
+	          query = query.setParameter("status", orderSearch.getOrderStatus());
+	      }
+	      if (StringUtils.hasText(orderSearch.getMemberName())) {
+	          query = query.setParameter("name", orderSearch.getMemberName());
+	      }
+	      return query.getResultList();
+	}
+	
+	public List<Order> findAllWithMemberRepository(){
+		return em.createQuery(
+				"select o from Order o"+
+				" join fetch o.member m"+
+				" join fetch o.delivery d",Order.class)
+				.getResultList();
+		
+		
+	}
+	
+	public List<Order> findAllWithItem(){
+		return em.createQuery("select distinct o from Order o"+
+								" join fetch o.member m"+
+								" join fetch o.delivery d"+
+								" join fetch o.orderItems oi"+
+								" join fetch oi.item i",Order.class).getResultList();
+	}
+	
+	public List<Order> findAllWithMemberDelivery(int offset,int limit){
+		return em.createQuery(
+				"select o from Order o"+
+				" join fetch o.member m"+
+				" join fetch o.delivery d",Order.class)
+				.setFirstResult(offset)
+				.setMaxResults(limit).
+				getResultList();
+				
+	}
+
 }
